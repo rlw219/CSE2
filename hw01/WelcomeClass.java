110b02748c79514b07281e69dcb7346bb1c10814
rlw219 <rlw219@lehigh.edu>
2015-08-30 16:44:11 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////
+//Rebecca Woods
+//hw01
+//Welcome Class Java Program
+//
+//  first compile the program
+//     javac WelcomeClass.java
+//  run the program
+//     java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+
+//  add main method
+    public static void main(String[] args)  {
+    
+//  print the following statement
+    System.out.println("   -----------");
+    System.out.println("   | WELCOME |");
+    System.out.println("   -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-R--L--W--2--1--9->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v  ");
+    System.out.println("Two years ago, I broke my femur playing tag");
+    System.out.println("while at a retreat.");
+    
+    //test/testtest
+    }
+}
